a723fb622c137e0709a5d089003030f21f225969
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-25 06:00:08 -0700

    Add command line args examples

--- 11-command-line-args.go
+++ 11-command-line-args.go
@@ -1,6 +1,6 @@
 /**
   * command-line-args.go
-  * A program showing how to use command-line arguments and flags
+  * A program showing how to use command-line arguments and flags in Go
   */
 
 package main
@@ -10,8 +10,9 @@ import (
 	"os"
 )
 
-var conf string
-var limit int
+var str string
+var num int
+var help bool
 
 func main() {
 
@@ -19,22 +20,39 @@ func main() {
 	// first argument in list is program itself
 	num_args := len( os.Args )
 	
+	// check if received any command line arguments
+	if num_args < 2 {
+		fmt.Println(">>> No args passed in")
+	}
+	
 	// flag package provides parsing of command-line parameters
-	flag.StringVar(&conf, "conf", "default value", "text description")
-	flag.IntVar(&limit, "limit", 5, "text description")
+	// this example we create global variables and then pass them in 
+	// as pointers which BoolVar, StringVar and IntVar set as values
+	flag.StringVar(&str, "str", "default value", "text description")
+	flag.IntVar(&num, "num", 5, "text description")
+	flag.BoolVar(&help, "help", false, "Display Help")
 	flag.Parse()
 
-	fmt.Println("Conf: ", conf)
-	fmt.Println("Limit: ", limit)
+	// check if help was called explicitly
+	if help {
+		fmt.Println("Display help screen")
+		os.Exit(1)
+	}
+	
+	fmt.Println("String: ", str)
+	fmt.Println("Number: ", num)
 
 	// last command-line argument
 	fmt.Println("Last Item: ", os.Args[num_args-1])
 }
 
 // $ go run command-line-args.go
-// >> Conf: default value
-// >> Conf: 5
+// >> String: default value
+// >> Number: 5
 
 // $ go run command-line-args.go --conf=Foo --limit=8 filename
-// >> Conf: Foo
-// >> Conf: 8
+// >> String: Foo
+// >> Number: 8
+
+// Try passing in invalid values, invalid flags and other tests
+// The flag package provides a lot to help build command-line programs
