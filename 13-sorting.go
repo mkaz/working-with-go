c2c516d17c213c5f546413e6e639ab3ce6e04f84
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-25 19:32:41 -0700

    gofmt spaces and whitespace

--- 13-sorting.go
+++ 13-sorting.go
@@ -1,9 +1,9 @@
 /**
-  * sorting.go
-  *
-  * Example program on how to sort various arrays and maps
-  *
-  */
+ * sorting.go
+ *
+ * Example program on how to sort various arrays and maps
+ *
+ */
 
 package main
 
@@ -17,42 +17,42 @@ func main() {
 	// sort alphabetical
 	abc := []string{"jkl", "ghi", "abc", "def"}
 	nums := []int{4, 2, 12, 5, 1, 3}
-	
+
 	// show default slices out of order
-	fmt.Println( "Raw ABC : ", abc )
-	fmt.Println( "Raw Nums: ", nums )
-	
+	fmt.Println("Raw ABC : ", abc)
+	fmt.Println("Raw Nums: ", nums)
+
 	// sort using Strings() method
-	sort.Strings( abc )
-	fmt.Println( "Sorted ABC:", abc )
-	
+	sort.Strings(abc)
+	fmt.Println("Sorted ABC:", abc)
+
 	// sort using Ints() method
-	sort.Ints( nums )
-	fmt.Println( "Sorted Nums:", nums )
-	
+	sort.Ints(nums)
+	fmt.Println("Sorted Nums:", nums)
+
 	// reverse sort, need to cast abc as a StringSlice reverse and sort
-	sort.Sort( sort.Reverse( sort.StringSlice( abc ) ) )
-	fmt.Println( "Reverse ABC:", abc )
-	
+	sort.Sort(sort.Reverse(sort.StringSlice(abc)))
+	fmt.Println("Reverse ABC:", abc)
+
 	//-------------------------------------------------
 	// Sort Maps
-	//-------------------------------------------------	
+	//-------------------------------------------------
 	// sort map by key
 	hash := map[string]int{
-		  "c": 3,
-		  "a": 1,
-		  "b": 2,
-		  "e": 5,		 
-		  "d": 4,
+		"c": 3,
+		"a": 1,
+		"b": 2,
+		"e": 5,
+		"d": 4,
 	}
 
 	// view map to see its out of order
 	for k, v := range hash {
-		fmt.Printf ( "%s => %v\n", k, v )
+		fmt.Printf("%s => %v\n", k, v)
 	}
 
 	// to sort, create array of keys and sort array
-	keys := make([]string, 0, len( hash ))
+	keys := make([]string, 0, len(hash))
 	for k := range hash {
 		keys = append(keys, k)
 	}
@@ -60,10 +60,7 @@ func main() {
 
 	// display hash by looping ordered keys
 	for i := range keys {
-		fmt.Printf ( "%s => %v\n", keys[i], hash[keys[i]] )
+		fmt.Printf("%s => %v\n", keys[i], hash[keys[i]])
 	}
 
-
 }
-
-
---
+++ 14-json.go
@@ -0,0 +1,63 @@
+/**
+  * json.go
+  *
+  * Example program on using JSON with Go
+  * As you might notice a trend, there is a standard library for it.
+  *
+  */
+
+package main
+
+import (
+	    "encoding/json"
+    "fmt"
+	"io/ioutil"
+
+)
+
+// Create a struct to match the format of JSON
+type Person struct {
+	Name, City string
+}
+
+// create a single instance of Person
+var person Person
+
+// create an array of Person types
+var people []Person
+
+func main() {
+
+    // JSON string to parse, see below for example read in from file
+    json_str := "{ \"Name\": \"Marcus\", \"City\": \"San Jose\"}"
+
+	// JSON Unmarshal command takes []byte, so string needs to be cast
+	// The second parameter is a pointer to the struct that matches format
+	if err := json.Unmarshal([]byte( json_str ), &person); err != nil {
+		fmt.Println("Error parsing JSON: ", err)
+	}
+
+	// output result
+	fmt.Printf("Name: %v, City: %v\n", person.Name, person.City)
+	
+	// read json in from a file
+	file, err := ioutil.ReadFile( "./extras/names.json" )
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+	
+	// the names.json file has an array of person objects, so read into people 
+	if err := json.Unmarshal( file, &people ); err != nil {
+		fmt.Println("Error parsing JSON", err)
+	}
+
+	// output result
+	fmt.Println( people )
+	
+	// encoding a Go object into JSON is simply using the Marshal command
+	json, err := json.Marshal(people)
+	if err != nil {
+		fmt.Println("JSON Encoding Error", err )
+	}
+	fmt.Println( string(json) )
+}
