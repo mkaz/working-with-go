c4d08ac7e591a1e66cdb0b5aa0f0549ed365b3a3
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-26 06:51:53 -0700

    add guessing game example

---
+++ 17-guess-game.go
@@ -0,0 +1,51 @@
+/**
+ * guess-game.go
+ *
+ * Guess a number between 1-100
+ *
+ */
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+
+func main() {
+	// variable to store guess
+	var guess int
+
+	// variable to store number of guesses
+	var count int
+	
+	// new random seed
+	rand.Seed(time.Now().UnixNano())
+
+	// pick number to guess, add 1 since Intn gives [0,99]
+	num := rand.Intn(100) + 1
+
+	fmt.Println(">> I'm thinking of a number between 1-100 << ")
+	
+	// guessing loop
+	for {
+		// prompt user for guess
+		fmt.Print("Guess: ")
+	    _, err := fmt.Scanf("%d", &guess)
+		if err == nil {
+			count += 1 // increment guess counter
+			if guess > num {
+				fmt.Println(" >> Too high ")
+			} else if guess < num {
+				fmt.Println(" >> Too low ")
+			} else {
+				fmt.Printf("Correct! It took you %d guesses!\n", count)
+				break
+			}
+		} else { // an error with input
+			fmt.Println(">> Please input a number")
+		}		
+	}	
+}
