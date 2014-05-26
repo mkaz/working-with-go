b6252140fcc44491dd061b5bd86922dd3cde11af
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-25 19:34:20 -0700

    add json encode/decode examples

--- 14-json.go
+++ 14-json.go
@@ -1,18 +1,17 @@
 /**
-  * json.go
-  *
-  * Example program on using JSON with Go
-  * As you might notice a trend, there is a standard library for it.
-  *
-  */
+ * json.go
+ *
+ * Example program on using JSON with Go
+ * As you might notice a trend, there is a standard library for it.
+ *
+ */
 
 package main
 
 import (
-	    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
-
 )
 
 // Create a struct to match the format of JSON
@@ -28,36 +27,36 @@ var people []Person
 
 func main() {
 
-    // JSON string to parse, see below for example read in from file
-    json_str := "{ \"Name\": \"Marcus\", \"City\": \"San Jose\"}"
+	// JSON string to parse, see below for example read in from file
+	json_str := "{ \"Name\": \"Marcus\", \"City\": \"San Jose\"}"
 
 	// JSON Unmarshal command takes []byte, so string needs to be cast
 	// The second parameter is a pointer to the struct that matches format
-	if err := json.Unmarshal([]byte( json_str ), &person); err != nil {
+	if err := json.Unmarshal([]byte(json_str), &person); err != nil {
 		fmt.Println("Error parsing JSON: ", err)
 	}
 
 	// output result
 	fmt.Printf("Name: %v, City: %v\n", person.Name, person.City)
-	
+
 	// read json in from a file
-	file, err := ioutil.ReadFile( "./extras/names.json" )
+	file, err := ioutil.ReadFile("./extras/names.json")
 	if err != nil {
 		fmt.Println("Error reading file")
 	}
-	
-	// the names.json file has an array of person objects, so read into people 
-	if err := json.Unmarshal( file, &people ); err != nil {
+
+	// the names.json file has an array of person objects, so read into people
+	if err := json.Unmarshal(file, &people); err != nil {
 		fmt.Println("Error parsing JSON", err)
 	}
 
 	// output result
-	fmt.Println( people )
-	
+	fmt.Println(people)
+
 	// encoding a Go object into JSON is simply using the Marshal command
 	json, err := json.Marshal(people)
 	if err != nil {
-		fmt.Println("JSON Encoding Error", err )
+		fmt.Println("JSON Encoding Error", err)
 	}
-	fmt.Println( string(json) )
+	fmt.Println(string(json))
 }
