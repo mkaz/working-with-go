ec9b3cc02678aba63c433a01da49bbc15e782fb8
Marcus Kazmierczak <marcus@mkaz.com>
2014-05-26 06:16:30 -0700

    update web server with some additional comments and clarification

--- 15-web-server.go
+++ 15-web-server.go
@@ -1,8 +1,12 @@
 /**
  * web-server.go
  *
- * A program example web server
+ * An example web server program, the standard library net/http provides
+ * a pretty powerful set of tools which makes it relatively easy to create
+ * your own web server.
  *
+ * See my Lanyon project: https://github.com/mkaz/lanyon for a more complete
+ * server which converts markdown to html
  */
 
 package main
@@ -14,22 +18,25 @@ import (
 )
 
 func main() {
-	// define function to handle requests
+	// associate URLs requested to functions that handle requests
 	http.HandleFunc("/hello", helloRequest)
 	http.HandleFunc("/", getRequest)
 
 	// start web server
-	fmt.Println("Listening on http://localhost:9999/")
+	log.Println("Listening on http://localhost:9999/")
 	log.Fatal(http.ListenAndServe(":9999", nil))
 }
 
 // basic handler for /hello request
 func helloRequest(w http.ResponseWriter, r *http.Request) {
+
+	// Fprint writes to a writer, in this case the http response
 	fmt.Fprint(w, "Hallo Welt")
 	return
 }
 
-// setup as default file server
+// this function simply serves up the file requested, or
+// an index list if a directory is requested
 func getRequest(w http.ResponseWriter, r *http.Request) {
 	file_requested := "./" + r.URL.Path
 	http.ServeFile(w, r, file_requested)
